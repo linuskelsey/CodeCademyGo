900a304accfba1005e622d441e0f4de8dbd05a4b
linuskelsey <alfred.linus.kelsey@gmail.com>
2024-01-27 15:25:09 +0000

    Add lessons from variables on strings and type inferrence

---
+++ variables/lessons/strings.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Define a string variable
+	// called favoriteSnack
+	var favoriteSnack string
+	// Assign a value to
+	// favoriteSnack
+	favoriteSnack = "Sour cream and chive combo mix from Sainsbury's"
+	// Print out the message
+	// "My favorite snack is "
+	// followed by the value in
+	// favoriteSnack
+	fmt.Println("My favourite snack is " + favoriteSnack + ".")
+}
